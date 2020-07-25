0fcb190834f2ef21b54d39cf81f85e108be7a6a6
Jose Useche <jose1useche@gmail.com>
2020-07-25 14:56:02 -0400

    72. Understanding-More on View Encapsulation

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  // encapsulation: ViewEncapsulation.None, //Angular por defecto encapsula los tags de sus componentes para que los estilos que se
+  //apliquen pues sean parte del componente en sí y nada mas. Esto lo hace Angular colocando un atributo único a los tags para que
+  //los estilos sean aplicados a éstos y no de forma global. Sin embargo, la propiedad encapsulation: ViewEncapsulation.None, nos
+  //permite eliminar estos atributos a los tags y además todos aquellos estilos que incluyamos en el .CSS de este componente serán
+  //ejecutados globalmente (ver el .css de este componente como ejemplo).
 })
 export class ServerElementComponent implements OnInit {
   @Input('srvElement') element: {type: string, name: string, content: string};
