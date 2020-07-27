96aaf2dd939b8c034d40bf125c7700fee8297320
Jose Useche <jose1useche@gmail.com>
2020-07-27 09:30:42 -0400

    Cleaning the work area (RETURNING TO RECIPE PROYECT!!!)

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2020/02/01/05/53/thai-food-4809563_960_720.jpg'),
+    new Recipe('A Second Recipe', 'This is another test', 'https://c1.peakpx.com/wallpaper/225/871/811/noodles-pad-thai-thai-food-asian-wallpaper-preview.jpg'),
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
