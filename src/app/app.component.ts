5a2db3d20172be11da4b463cb2bbaabbe79e3be2
Jose Useche <jose1useche@gmail.com>
2020-08-09 17:39:11 -0400

    109-110. Understanding the Hierarchical Injector.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountsService } from './accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AccountsService], //NOTE: Hierarchical Injector remember that. If you use the providers on his children you create a new
+    //instance of the object.
 })
-export class AppComponent {
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active'
-    },
-    {
-      name: 'Testaccount',
-      status: 'inactive'
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown'
-    }
-  ];
+export class AppComponent implements OnInit {
+  accounts: {name: string, status: string}[] = [];
 
-  onAccountAdded(newAccount: {name: string, status: string}) {
-    this.accounts.push(newAccount);
+  constructor(private accountsService: AccountsService) {}
+
+  ngOnInit(): void {
+    this.accounts = this.accountsService.accounts;
   }
 
-  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
-    this.accounts[updateInfo.id].status = updateInfo.newStatus;
-  }
 }
