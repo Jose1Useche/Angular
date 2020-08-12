c35897d06725fb4da66934f7639df592d9f49c2f
Jose Useche <jose1useche@gmail.com>
2020-08-12 12:42:24 -0400

    114. Assignment Solution.

--- src/app/loggin.service.ts
+++
@@ -1,5 +0,0 @@
-export class LogginService { //NOTE: A service is just a normal TS class.
-    logStatusChange(status: string) {
-        console.log('A server status changed, new status: ' + status);
-    }
-}
