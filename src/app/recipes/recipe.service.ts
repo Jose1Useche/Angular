f5ad4442542fcdf0757aa6f96f1605c7d22fefa0
Jose Useche <jose1useche@gmail.com>
2020-08-15 16:35:44 -0400

    118.using a Service for Cross-Component Communication.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import { Recipe } from './/recipe.model'
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();    
+
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2020/02/01/05/53/thai-food-4809563_960_720.jpg'),
         new Recipe('A Second Recipe', 'This is another test', 'https://c1.peakpx.com/wallpaper/225/871/811/noodles-pad-thai-thai-food-asian-wallpaper-preview.jpg'),
