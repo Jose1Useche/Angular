e9ea5860b1453b9aad62c565c44e09adacfa261b
Jose Useche <jose1useche@gmail.com>
2020-09-01 16:02:05 -0400

    173. Errors & Completion.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { interval, Subscription, Observable } from 'rxjs'
+import { error } from '@angular/compiler/src/util';
 
 @Component({
   selector: 'app-home',
@@ -21,12 +22,23 @@ export class HomeComponent implements OnInit, OnDestroy {
       let count = 0;
       setInterval(() => {
         observer.next(count);
-        count++;//complete or error
+        if(count === 2) {
+          observer.complete();
+        }
+        if(count >= 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        }
+        count++;
       }, 1000);
     });
 
     this.firstObsSubscription = customIntervalObservable.subscribe(data => {
       console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message);
+    }, () => {
+      alert('The task is completed and this function doesn\'t\ need an argument because is the complete part!');
     });
   }
 
