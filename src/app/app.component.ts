71c298b7a23d217d5eb137310638da82dceba333
Jose Useche <jose1useche@gmail.com>
2020-09-03 09:33:29 -0400

    179. Improving the Reactive Service with Observables (Subjects IN THE RECIPE'S PROJECT)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,10 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { UserService } from './user.service';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  userActivated = false;
-  private activatedSub: Subscription;
-
-  constructor(private userService: UserService) {}
-  
-  ngOnInit() {
-    this.activatedSub = this.userService.activatedEmitter.subscribe(didActivated => {
-      this.userActivated = didActivated;
-    });
-  }
-
-  ngOnDestroy(): void {
-    this.activatedSub.unsubscribe();
-  }
+export class AppComponent {
+  loadedFeature = 'recipe';
 }
--- src/app/user.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable, EventEmitter } from '@angular/core';
-import { Subject } from 'rxjs';
-
-@Injectable({providedIn: 'root'}) //This is a short way to provide this service into the app.module.ts
-export class UserService {
-    // activatedEmitter = new EventEmitter<boolean>();
-
-    activatedEmitter = new Subject<boolean>();
-}
