02f4f1a4851d1a125920e78f8eaee249e08631d6
Jose Useche <jose1useche@gmail.com>
2020-09-27 21:06:37 -0400

    247. Pure and Impure Pipes (or: How to fix the Filter Pipe).

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,12 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  pure: false //Se debe tener cuidado con la pureza del pipe. Si se hace "impuro", éste se va a ejecutar cada vez que parte del DOM que
+              //esté relacionado con este pipe sufra algún cambio, como agregar un nuevo servidor por ejemplo. Esto puede repercutir
+              //considerablemente con el performance. Así que, hacerlo impuro solo si es extrictamente necesario.
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any, filterString: string, propName: string) {
-    if(value.length === 0) {
+  transform(value: any, filterString: string, propName: string): any {
+    if(value.length === 0 || filterString === '') {
       return value;
     }
     const resultArray = [];
