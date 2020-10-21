3814ef6b37e4aca634bd290b9b11754dfe79b7b9
Jose Useche <jose1useche@gmail.com>
2020-10-21 16:01:32 -0400

    299. Reflecting the Auth State in the UI.

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthResponseData, AuthService } from './auth.service';
 
@@ -13,7 +14,7 @@ export class AuthComponent implements OnInit {
   isLoading = false;
   activeError: string = null;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -60,6 +61,7 @@ export class AuthComponent implements OnInit {
     authObs.subscribe(resData => {
       console.log(resData);
       this.isLoading = false;
+      this.router.navigate(['/recipes']);
     }, errorMessage => {
       console.log(errorMessage);
       this.activeError = errorMessage;
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,18 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-    constructor(private dataSotargeService: DataStorageService) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+    isAuthenticated = false;
+    private userSub: Subscription;
+
+    constructor(private dataSotargeService: DataStorageService, private authService: AuthService) {}
+    
+    ngOnInit(): void {
+        this.userSub = this.authService.user.subscribe(user => {
+            this.isAuthenticated = !!user//It's the same !user ? false : true;
+            console.log(!user);
+            console.log(!!user);
+        });
+    }
 
     onSaveData() {
         this.dataSotargeService.storeRecipes();
     }
-
+    
     onFetchData() {
         this.dataSotargeService.fetchRecipes().subscribe();
+    }
+
+    ngOnDestroy(): void {
+        this.userSub.unsubscribe();
     }
 }
