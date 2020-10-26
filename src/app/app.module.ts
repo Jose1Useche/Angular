7f373b8a5d7d1be509fd13a212904c828c06dc56
Jose Useche <jose1useche@gmail.com>
2020-10-26 10:33:14 -0400

    325. The ShoppingList Feature Module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive'
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
@@ -21,13 +18,12 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AuthGuard } from './auth/auth.guard';
 import { AlertComponent } from './shared/alert/alert.component';
 import { RecipesModule } from './recipes/recipes.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     DropdownDirective,
     AuthComponent,
     LoadingSpinnerComponent,
@@ -39,7 +35,8 @@ import { RecipesModule } from './recipes/recipes.module';
     ReactiveFormsModule,//We need this to sincronize our form with the html form.
     HttpClientModule, //This is crucial to unlock the http client functionality in our application.
     AppRoutingModule,
-    RecipesModule
+    RecipesModule,
+    ShoppingListModule
   ],
   providers: [
       ShoppingListService, 
---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+
+import { ShoppingListComponent } from './shopping-list.component';
+import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { ShoppingListRoutingModule } from './shopping-list-routing.module';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+    declarations: [
+        ShoppingListComponent,
+        ShoppingEditComponent,
+    ],
+    imports: [
+        RouterModule, // con esto corrijo los errores de enrutamiento
+        CommonModule, // con esto habilito los ngIf ngFor
+        FormsModule, //habilito la creacion de formularios.
+        ShoppingListRoutingModule
+    ]
+})
+export class ShoppingListModule { }
