b9acb922f3d20534eff048b9e8be875cff926ad3
Jose Useche <jose1useche@gmail.com>
2020-10-26 12:20:25 -0400

    328. Adding an Auth Feature Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AuthComponent } from './auth/auth.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent,
     //Las declaraciones se hacen solo una vez, no importa si estas en módulos distintos; estos 3 ya estan declarados en SharedModule
     // DropdownDirective,
     // LoadingSpinnerComponent,
@@ -24,12 +23,13 @@ import { CoreModule } from './core.module';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,//We need this to sincronize our form with the html form.
+    //Se movió a auth.module.ts // FormsModule,//We need this to sincronize our form with the html form.
+    //Se movió a auth.module.ts // ReactiveFormsModule,//We need this to sincronize our form with the html form.
     HttpClientModule, //This is crucial to unlock the http client functionality in our application.
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
+    AuthModule,
     SharedModule,
     CoreModule // aquí metemos toda la seccion de los providers: []
   ],
